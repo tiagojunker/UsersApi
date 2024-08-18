b59861ee344474a07e78d35442bc66fa631eae3e
Tiago Junker <tiago99junker@gmail.com>
2024-08-18 15:46:40 -0300

    First Commit

---
+++ src/test/java/com/UserApi/Api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserApi.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
