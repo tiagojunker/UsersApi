132696faebce34176989e549b84b6966c6c3ddca
Tiago Junker <tiago99junker@gmail.com>
2024-08-19 12:30:48 -0300

    Feat: Added Bean Validators to the User Entity, added also custom exceptionhandler to bean validators

--- src/main/java/com/UserApi/Api/Entities/User.java
+++ src/main/java/com/UserApi/Api/Entities/User.java
@@ -1,11 +1,14 @@
 package com.UserApi.Api.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
@@ -22,15 +25,21 @@ public class User {
     private Integer id;
 
     @Column
+    @NotEmpty(message = "Name can't be empty!")
     private String name;
 
     @Column(unique = true, length = 11)
+    @NotEmpty(message = "CPF can't be empty!")
+    @CPF(message = "CPF must be valid!")
     private String cpf;
 
     @Column(unique = true)
+    @NotEmpty(message = "Email can't be empty!")
+    @Email(message = "Email must be valid!")
     private String email;
 
     @Column
+    @NotNull(message = "Birth date can't be empty!")
     private LocalDate birthDate;
 
 }
---
+++ src/main/java/com/UserApi/Api/Exceptions/ControllerAdvice/BeanValidationGlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.UserApi.Api.Exceptions.ControllerAdvice;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class BeanValidationGlobalExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
+        Set<ConstraintViolation<?>> violations = ex.getConstraintViolations();
+        Map<String, String> errors = new HashMap<>();
+
+        if (violations != null) {
+            errors = violations.stream()
+                    .collect(Collectors.toMap(
+                            violation -> violation.getPropertyPath().toString(),
+                            violation -> violation.getMessage(),
+                            (existing, replacement) -> existing
+                    ));
+        }
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException exception) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
